9f1b63f7c7bd63163f5c9c3ad1ecac56448ff651
mdsjjorge <mdsousa.jorge@gmail.com>
2022-09-08 11:40:43 -0300

    adc calculadora de soma

---
+++ Calc/src/calculator/Num.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author mdsjj
+ */
+public class Num {
+    public int value;
+
+    public void isNum() {
+        this.value = 0;
+        // return this.value = 0;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+}
+
